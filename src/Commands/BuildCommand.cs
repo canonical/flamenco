be56c3ae83af57cb89485ec86f7c7a717e7c1825
Dominik Viererbe <dominik.viererbe@canonical.com>
2024-05-17 12:36:31 +0200

    feat: add environment variable for build cmd flags

--- src/Commands/BuildCommand.cs
+++ src/Commands/BuildCommand.cs
@@ -9,15 +9,28 @@ public class BuildCommand : Command
         var sourceDirectoryOption = new Option<DirectoryInfo>(
             name: "--source-directory",
             description: "The directory that the build tool uses to produce its targets.",
-            getDefaultValue:  () => new DirectoryInfo("src"));
+            getDefaultValue:  GetDirectoryFromEnvironmentOrDefault(
+                EnvironmentVariableNames.DestinationDirectory, 
+                defaultPath: "src"));
         
         var destinationDirectoryOption = new Option<DirectoryInfo>(
             name: "--destination-directory",
             description: "The directory where the targets are build.",
-            getDefaultValue: () => new DirectoryInfo("dist"));
+            getDefaultValue: GetDirectoryFromEnvironmentOrDefault(
+                EnvironmentVariableNames.DestinationDirectory, 
+                defaultPath: "dist"));
 
         AddGlobalOption(sourceDirectoryOption);
         AddGlobalOption(destinationDirectoryOption);
         AddCommand(new BuildDebianTarballCommand());
+    }
+    
+    private Func<DirectoryInfo> GetDirectoryFromEnvironmentOrDefault(string environmentVariableName, string defaultPath)
+    {
+        return () =>
+        {
+            string path = Environment.GetEnvironmentVariable(environmentVariableName) ?? defaultPath;
+            return new DirectoryInfo(path);
+        };
     }
 }
---
+++ src/EnvironmentVariableNames.cs
@@ -0,0 +1,7 @@
+namespace Flamenco;
+
+public static class EnvironmentVariableNames
+{
+    public const string SourceDirectory = "FLAMENCO_SOURCE_DIRECTORY";
+    public const string DestinationDirectory = "FLAMENCO_DESTINATION_DIRECTORY";
+}
