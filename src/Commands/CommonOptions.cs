1673e8e4949af94e1347c66ee72cbfbf53d381a1
Dominik Viererbe <dominik.viererbe@canonical.com>
2024-06-05 23:36:06 +0300

    docs: add default value to option help text
    Signed-off-by: Dominik Viererbe <dominik.viererbe@canonical.com>

--- src/Commands/CommonOptions.cs
+++ src/Commands/CommonOptions.cs
@@ -1,5 +1,4 @@
 using System.CommandLine;
-using System.Diagnostics.CodeAnalysis;
 using Flamenco.Packaging;
 
 namespace Flamenco.Commands;
@@ -8,21 +7,21 @@ public static class CommonOptions
 {
     public static readonly Option<DirectoryInfo?> SourceDirectoryOption = new(
         name: "--source-directory",
-        description: "The directory that the build tool uses to produce its targets.")
+        description: "The directory that the build tool uses to produce its targets. [default: ./src]")
         {
             Arity = ArgumentArity.ExactlyOne
         };
 
     public static readonly Option<DirectoryInfo?> DestinationDirectoryOption = new(
         name: "--destination-directory",
-        description: "The directory where the targets are build.")
+        description: "The directory where the targets are build. [default: ./dist]")
         {
             Arity = ArgumentArity.ExactlyOne,
         };
     
     public static readonly Option<Tarball.CompressionMethod> DebianTarballCompressionMethod = new (
         name: "--debian-tarball-compression-method",
-        description: "The compression method used to create debian tar archives.")
+        description: "The compression method used to create debian tar archives. [default: xz]")
     {
         Arity = ArgumentArity.ExactlyOne,
     };
