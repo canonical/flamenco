5f16b02ecf42ecb7bf4899b21513273fb1f8c0ee
Mateus Rodrigues de Morais <mateus.morais@canonical.com>
2025-11-28 15:53:04 -0300

    refactor: handle exception during Git clone operation

--- src/Flamenco.Packaging/ExternalSources/GitExternalSource.cs
+++ src/Flamenco.Packaging/ExternalSources/GitExternalSource.cs
@@ -1,3 +1,4 @@
+using System.Collections.Immutable;
 using LibGit2Sharp;
 
 namespace Flamenco.Packaging.ExternalSources;
@@ -13,13 +14,43 @@ public class GitExternalSource(string repository, string? commitish = null) : Ex
 
         if (cancellationToken.IsCancellationRequested) return result.WithAnnotation(new OperationCanceled());
 
-        await Task.Run(() => LibGit2Sharp.Repository.Clone(Repository, destinationDirectory, new CloneOptions
+        try
         {
-            BranchName = Commitish
-        }), cancellationToken);
+            await Task.Run(() =>
+            {
+                LibGit2Sharp.Repository.Clone(Repository, destinationDirectory);
 
-        Directory.Delete(Path.Join(destinationDirectory, ".git"), recursive: true);
+                if (string.IsNullOrEmpty(Commitish)) return;
+
+                using var repo = new Repository(destinationDirectory);
+                Commands.Checkout(repo, Commitish);
+            }, cancellationToken);
+        }
+        catch (OperationCanceledException)
+        {
+            return result.WithAnnotation(new OperationCanceled());
+        }
+        catch (Exception ex)
+        {
+            return new GitCloneFailed(Repository, Commitish, ex);
+        }
+
+        var gitDir = Path.Join(destinationDirectory, ".git");
+        if (Directory.Exists(gitDir))
+        {
+            Directory.Delete(Path.Join(destinationDirectory, ".git"), recursive: true);
+        }
 
         return result;
     }
+
+    public class GitCloneFailed(
+        string repository,
+        string? commitish,
+        Exception exception)
+        : ErrorBase(
+            identifier: "FL0050",
+            title: "Git clone failed.",
+            message: $"Failed to clone the git repository '{repository}' at '{commitish ?? "default branch"}'",
+            innerAnnotations: ImmutableList.Create<IAnnotation>(new ExceptionalAnnotation(exception)));
 }
